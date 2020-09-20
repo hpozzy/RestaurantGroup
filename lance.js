f0767fee950d2c2278af3f039cc5aef6f54f6c95
Punchcode <punchcode@Punchcodes-MacBook-Pro.local>
2020-09-20 02:40:10 -0700

    Lance's html, css, js update

--- lance.js
+++ lance.js
@@ -1,3 +1,55 @@
+let tabs = document.querySelectorAll(".label7tabs")
+let contents = document.querySelectorAll(".label7divs")
+
+const handleClick = (e, btn) => {
+  e.preventDefault()
+  tabs.forEach((target) => {
+    target.classList.remove("activeTab")
+  })
+  e.currentTarget.classList.add("activeTab")
+  contents.forEach((x) => x.classList.remove("active"))
+  btn.classList.add("active")
+}
+
+tabs.forEach((target, i) => {
+  target.addEventListener("click", (e) => handleClick(e, contents[i]))
+})
+
+const label7story = `<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+nulla pariatur. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+anim id est laborum.</p>
+
+<img src="" alt="" id="ourStoryImage">
+
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+ea commodo consequat. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+nulla pariatur. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+nulla pariatur. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+anim id est laborum.</p>`
+
+document.querySelector("#label7mainStory").innerHTML = label7story
+
+const menuDivs = `<div id="menuSectionHeader">Our Dinner Selections...</div>
+
+<div id="menuAppetizerHeader" class="menuHeaders">Appetizers</div>
+<div id="menuAppetizers" class="menuMarginBottom"></div>
+
+<div id="menuEntreeHeader" class="menuHeaders">Entrees</div>
+<div id="menuEntrees" class="menuMarginBottom"></div>
+
+<div id="menuDessertHeader" class="menuHeaders">Desserts</div>
+<div id="menuDesserts" class="menuMarginBottom"></div>`
+
+document.querySelector("#label7mainMenu").innerHTML = menuDivs
+
 const appetizerMenu = fetch(
   "https://obscure-tundra-54269.herokuapp.com/fine-dining"
 )
@@ -15,10 +67,18 @@ const appetizerMenu = fetch(
                 </div>
                 <div class="menuInfo">
                   <div class="menuDescription">
-                    <p>${i.description}</p>
+                    <p>This dish is made with <i>${i.description}</i>. We use farm fresh ingredients.</p>
                   </div>
                   <div class="menuLabels">
-                    <button>${i.extra.spicy}</button><button>${i.extra.glutenfree}</button><button>${i.extra.vegetarian}</button>
+                    <div class="threeIcons">HA
+                      <span class="extraDetails">${i.extra.spicy}</span>
+                    </div>
+                    <div class="threeIcons">HA
+                      <span class="extraDetails">${i.extra.glutenfree}</span>
+                    </div>
+                    <div class="threeIcons">HA
+                      <span class="extraDetails">${i.extra.vegetarian}</span>
+                    </div>
                   </div>
                 </div>`
       })
@@ -43,10 +103,18 @@ const entreeMenu = fetch(
                 </div>
                 <div class="menuInfo">
                   <div class="menuDescription">
-                    <p>${i.description}</p>
+                    <p>This dish is made with <i>${i.description}</i>. We use farm fresh ingredients.</p>
                   </div>
                   <div class="menuLabels">
-                  <button>${i.extra.spicy}</button><button>${i.extra.glutenfree}</button><button>${i.extra.vegetarian}</button>
+                  <div class="threeIcons">HA
+                      <span class="extraDetails">${i.extra.spicy}</span>
+                    </div>
+                    <div class="threeIcons">HA
+                      <span class="extraDetails">${i.extra.glutenfree}</span>
+                    </div>
+                    <div class="threeIcons">HA
+                      <span class="extraDetails">${i.extra.vegetarian}</span>
+                    </div>
                   </div>
                 </div>`
       })
@@ -71,10 +139,18 @@ const dessertMenu = fetch(
                 </div>
                 <div class="menuInfo">
                   <div class="menuDescription">
-                    <p>${i.description}</p>
+                    <p>This dish is made with <i>${i.description}</i>. We use farm fresh ingredients.</p>
                   </div>
                   <div class="menuLabels">
-                  <button>${i.extra.spicy}</button><button>${i.extra.glutenfree}</button><button>${i.extra.vegetarian}</button>
+                  <div class="threeIcons">HA
+                      <span class="extraDetails">${i.extra.spicy}</span>
+                    </div>
+                    <div class="threeIcons">HA
+                      <span class="extraDetails">${i.extra.glutenfree}</span>
+                    </div>
+                    <div class="threeIcons">HA
+                      <span class="extraDetails">${i.extra.vegetarian}</span>
+                    </div>
                   </div>
                 </div>`
       })
@@ -82,19 +158,21 @@ const dessertMenu = fetch(
     document.querySelector("#menuDesserts").innerHTML = dessertSection
   })
 
-let tabs = document.querySelectorAll(".label7tabs")
-let contents = document.querySelectorAll(".label7divs")
+const label7reservation = `<form action="" method="GET">
+  <label for="fullname">Full Name</label><br />
+    <input class="form-input" type="text" id="fullName" name="fname" /><br />
+  <label for="numberofguest">Number of Guests</label><br />
+   <input class="form-input" type="text" id="numberOfGuest" name="guest" /><br />
+  <label for="date">Date</label><br />
+   <input class="form-input" type="text" id="reservationDate" name="rDate" /><br />
+  <label for="specialNotes">Special Notes</label><br />
+   <textarea class="form-input" name="specialNotes" id="specialNotes" cols="30" rows="8"></textarea> <br />
+  <label for="seating">Seating Preference</label><br />
+    <select class="form-input" id="seatingP" name="seating">
+      <option value="indoor">Indoor</option>
+      <option value="outdoor">Outdoor</option>
+    </select>
+  <br>
+<button id='reservationButton' type="button">Reserve Table</button>`
 
-const handleClick = (e, btn) => {
-  e.preventDefault()
-  tabs.forEach((target) => {
-    target.classList.remove("active-btn")
-  })
-  e.currentTarget.classList.add("active-btn")
-  contents.forEach((x) => x.classList.remove("active"))
-  btn.classList.add("active")
-}
-
-tabs.forEach((target, i) => {
-  target.addEventListener("click", (e) => handleClick(e, contents[i]))
-})
+document.querySelector("#label7reservation").innerHTML = label7reservation
